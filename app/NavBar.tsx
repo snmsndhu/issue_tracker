7ba7629ce4093c88c95efcad50486b33380bd407
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-11-20 12:28:54 -0500

    Navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import React from "react";
+import { FaBug } from "react-icons/fa";
 
 const NavBar = () => {
   return (
-    <nav>
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">Logo</Link>
-      <ul>
+      <ul className="flex space-x-6">
         <li>
           <Link href="/">Dashboard</Link>
         </li>
