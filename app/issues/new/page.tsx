5e809e06421dfeb2cccb852f393edf504ace3e3b
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-11-21 11:26:38 -0500

    form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,15 +1,28 @@
 "use client";
 import { Button, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
+import { useForm, Controller } from "react-hook-form";
 import "easymde/dist/easymde.min.css";
 
+interface IssueForm {
+  title: string;
+  description: string;
+}
+
 const NewIssuePage = () => {
+  const { register, control, handleSubmit } = useForm<IssueForm>();
   return (
     <div className="max-w-xl space-y-3">
       <TextField.Root>
-        <TextField.Input placeholder="Title" />
+        <TextField.Input placeholder="Title" {...register("title")} />
       </TextField.Root>
-      <SimpleMDE placeholder="description" />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="description" {...field} />
+        )}
+      />
       <Button>Submit New Issue</Button>
     </div>
   );
