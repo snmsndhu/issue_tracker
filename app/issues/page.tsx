2a83b2d5ffe7471abf808ab5731313f1b99ad14b
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-11-24 08:30:11 -0500

    Styled Link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Button, Table } from "@radix-ui/themes";
-import Link from "next/link";
+import { Table } from "@radix-ui/themes";
 import prisma from "@/prisma/client";
 import IssueStatusBadge from "../components/IssueStatusBadge";
+import Link from "../components/Link";
 import delay from "delay";
 import IssueAction from "./IssueAction";
 
