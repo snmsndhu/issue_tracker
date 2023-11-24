16eae6c438aa9e844f60062e7915a93071bb9855
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-11-24 09:13:17 -0500

    Refactoring

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,9 +1,5 @@
-import Link from "./Link";
-import ErrorMessage from "./ErrorMessage";
-import IssueStatusBadge from "./IssueStatusBadge";
-import Spinner from "./Spinner";
-
-export { Link };
-export { ErrorMessage };
-export { IssueStatusBadge };
-export { Spinner };
+export { default as Link } from "./Link";
+export { default as ErrorMessage } from "./ErrorMessage";
+export { default as IssueStatusBadge } from "./IssueStatusBadge";
+export { default as Spinner } from "./Spinner";
+export { default as Skeleton } from "./Skeleton";
