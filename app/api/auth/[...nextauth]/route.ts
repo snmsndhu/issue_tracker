65e157ccc0149e60316ab49966587feea6c711da
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-11-29 08:00:13 -0500

    adding prisma adapter

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -12,6 +12,9 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  session: {
+    strategy: "jwt",
+  },
 });
 
 export { handler as GET, handler as POST };
