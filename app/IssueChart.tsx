000e57a94817193074b67ffd39c1a5f6c84b6538
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-12-02 20:43:36 -0500

    Final layout

--- app/IssueChart.tsx
+++ app/IssueChart.tsx
@@ -1,21 +1,25 @@
 "use client";
+
 import { Card } from "@radix-ui/themes";
-import React from "react";
 import { ResponsiveContainer, BarChart, XAxis, YAxis, Bar } from "recharts";
+import React from "react";
+
 interface Props {
   open: number;
   inProgress: number;
   closed: number;
 }
+
 const IssueChart = ({ open, inProgress, closed }: Props) => {
   const data = [
-    { label: "open", value: open },
+    { label: "Open", value: open },
     { label: "In Progress", value: inProgress },
     { label: "Closed", value: closed },
   ];
+
   return (
     <Card>
-      <ResponsiveContainer width="100" height={300}>
+      <ResponsiveContainer width="100%" height={300}>
         <BarChart data={data}>
           <XAxis dataKey="label" />
           <YAxis />
