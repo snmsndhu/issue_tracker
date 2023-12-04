584f234a0fecc644d2535f797261f9d39ad2ac88
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-12-04 12:49:53 -0500

    Adding Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -25,3 +25,8 @@ export default async function Home() {
     </Grid>
   );
 }
+
+export const metadat: Metadata = {
+  title: "Issue Tracker - Dashboard",
+  description: "View a summary of project issues",
+};
